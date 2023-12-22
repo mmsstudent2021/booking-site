024213ef764f2e48e867a1994583e4ab939e9386
yeaungzaw <yeaungzaw.learner@gmail.com>
2023-12-22 20:46:37 +0630

    dev start

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,7 @@
 import React from "react";
 
 const App = () => {
-  return (
-    <div className="container">
-      <h1 className="text-3xl text-beautifulBlue font-heading font-bold">
-        Test
-      </h1>
-    </div>
-  );
+  return <div className="container"></div>;
 };
 
 export default App;
