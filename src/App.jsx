9dfd6b401525be36b294da9b21808d35165f4539
yeaungzaw <yeaungzaw.learner@gmail.com>
2023-12-27 14:42:03 +0630

    update folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import Home from "./pages/Home";
 
 const App = () => {
   return (
     <div>
-      <h1 className="container">Hello</h1>
+      <Home />
     </div>
   );
 };
