49e2e2964a973fff41487908655f5d1f3c967167
yeaungzaw <yeaungzaw.learner@gmail.com>
2023-12-27 14:28:26 +0630

    banner initial commit

--- src/components/HotelChain.jsx
+++ src/components/HotelChain.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import HotelChainNav from "./HotelChainNav";
+import Banner from "./Banner";
 
 const HotelChain = () => {
   return (
     <div>
       <HotelChainNav />
+      <Banner />
     </div>
   );
 };
