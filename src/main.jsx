a6906c79b53352f7631177c70c85d28f25002659
swanhtetthetpaing <swanhtetthetpaing1@gmail.com>
2024-01-04 10:38:57 +0630

    glide and preline import update

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import ThemeProvider from "./contexts/ThemeContext.jsx";
+import "../node_modules/preline/dist/preline.js"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <ThemeProvider>
