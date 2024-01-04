875042735081ea18a0214be8289755a8c41badfa
swanhtetthetpaing <swanhtetthetpaing1@gmail.com>
2024-01-04 12:25:08 +0630

    router and Link finish

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,20 @@ import { ThemeContext } from "./contexts/ThemeContext";
 import Home from "./pages/Home";
 import Contact from "./pages/Contact";
 import Chain from "./pages/Chain";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const App = () => {
   const {theme} = useContext(ThemeContext);
   return (
     <div className={`${theme === "dark" && "dark"}`}>
       <div className="dark:bg-body-dark">
-        <Contact />
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/hotel-chain" element={<Chain />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </Router>
       </div>
     </div>
   );
